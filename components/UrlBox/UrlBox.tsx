ed8f31ae97411049a4a090489db83aa2bf0388d0
rnjshippo <tkstka0023@naver.com>
2020-07-08 02:10:57 +0900

    snackbar 생성작업
    material-ui snackbar 생성중

--- components/UrlBox/UrlBox.tsx
+++ components/UrlBox/UrlBox.tsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
-import Link from 'next/link';
 import styles from './UrlBox.module.scss';
 import * as service from '../../services/index';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { TYPE_COMPRESS, TYPE_COPY, BASE_URL } from '../../lib/util'
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
+
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const successMsg: string = "URL이 성공적으로 단축되었습니다.";
+const failMsg: string = "유효하지 않은 URL입니다.";
+const errorMsg: string = "URL 변환 중 에러가 발생했습니다. 다시 한번 시도해주세요.";
 
 interface Props {
   setFullUrl(url: string): void;
@@ -23,16 +32,23 @@ interface compressResponse {
 
 const Formbox = ({ resultUrl, fullUrl, buttonType, setFullUrl, setButtonType, setResultUrl }: Props) => {
 
+  /* 레이어 팝업 show 여부 state */
+  const [popupInfo, setPopupInfo] = useState({
+    visible: false,
+    success: false,
+    error: false
+  });
 
-  const [visible, setVisible] = useState(false)
+  /* clear 버튼 show 여부 state */
+  const [showClearButton, setShowClearButton] = useState(false)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
 
     if (e?.target?.value?.trim() === "") {
-      setVisible(false);
+      setShowClearButton(false);
     } else {
-      setVisible(true);
+      setShowClearButton(true);
     }
     setFullUrl(e?.target?.value?.trim());
     setButtonType(TYPE_COMPRESS)
@@ -51,26 +67,55 @@ const Formbox = ({ resultUrl, fullUrl, buttonType, setFullUrl, setButtonType, se
 
   const clearInputBox = () => {
     setFullUrl("");
-    setVisible(false);
+    setShowClearButton(false);
   }
 
   const handleSubmit = async (e: React.SyntheticEvent<HTMLElement>) => {
     e.preventDefault();
-    if (fullUrl.trim() === "") return;
-    let regix = '(http(s)?:\/\/)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}'
-    //TODO : 정규식으로 비정상 url 체크 후 alert 구현
+    const inputUrl: string = fullUrl.trim();
+    if (inputUrl === "") return;
 
-    let { data: { shortUrl }, status }: compressResponse = await service.url.compressFullUrl(fullUrl);
-    console.log(status, shortUrl)
-    if (status === 200) {
-      setButtonType(TYPE_COPY);
-      setResultUrl(BASE_URL + shortUrl);
-      setFullUrl(BASE_URL + shortUrl);
+    const regex = RegExp(/^(((http(s?))\:\/\/)?)([0-9a-zA-Z\-]+\.)+[a-zA-Z]{2,6}(\:[0-9]+)?(\/\S*)?$/);
+    const vaild: boolean = regex.test(inputUrl);
+
+    if (vaild) {
+      //TODO : 정규식으로 비정상 url 체크 후 popup 구현
+      let { data: { shortUrl }, status }: compressResponse = await service.url.compressFullUrl(fullUrl);
+      console.log(status, shortUrl)
+      if (status === 200) {
+        setButtonType(TYPE_COPY);
+        setResultUrl(BASE_URL + shortUrl);
+        setFullUrl(BASE_URL + shortUrl);
+
+        //TODO 성공시 팝업 구현
+        setPopupInfo({ visible: true, success: true, error: false });
+
+      } else {
+        console.error("변환 실패"); //TODO : 변환 실패 처리 
+        setFullUrl("");
+        setPopupInfo({ visible: true, success: false, error: true });
+      }
     } else {
-      setFullUrl("");
-      console.error("Error 변환 실패"); //TODO : 변환 실패 처리 
+      //TODO :: 비정상 Url 체크 후 팝업 구현
+      console.log("유효하지 않은 URL입니다.");
+      setPopupInfo({ visible: true, success: false, error: false });
     }
   }
+
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event: React.SyntheticEvent | React.MouseEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
     <>
       <div className={styles.url_div}>
@@ -78,7 +123,7 @@ const Formbox = ({ resultUrl, fullUrl, buttonType, setFullUrl, setButtonType, se
         <div className={styles.title_div2}>{subText}</div>
         <div className={styles.input_div}>
           <img src={"/icons/link.svg"} className={styles.link_btn}></img>
-          {visible ? <img src={"/icons/btn_clear.svg"} className={styles.clear_btn} onClick={clearInputBox} /> : null}
+          {showClearButton ? <img src={"/icons/btn_clear.svg"} className={styles.clear_btn} onClick={clearInputBox} /> : null}
           <input onChange={handleChange} onKeyPress={handleKeyPress} value={fullUrl} className={styles.url_box} spellCheck={"false"} placeholder={"http://"} />
           {buttonType === TYPE_COMPRESS ?
             <button
@@ -94,6 +139,14 @@ const Formbox = ({ resultUrl, fullUrl, buttonType, setFullUrl, setButtonType, se
         </div>
         {resultUrl === "" ? <span className={styles.subtext}>{}</span> : <span className={styles.subtext}>{successText}</span>}
       </div>
+      <button onClick={handleClick}>button</button>
+
+      <Snackbar open={open} autoHideDuration={2500} onClose={handleClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
+        <Alert onClose={handleClose} severity={popupInfo.success ? "success" : "error"}>
+          This is a success message!
+        </Alert>
+      </Snackbar>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,14 @@ import { Header, UrlBox } from '../components/index'
 import Head from 'next/head'
 import { TYPE_COMPRESS } from '../lib/util'
 
+
 export default () => {
 
   const [fullUrl, setFullUrl] = useState(String);
   const [resultUrl, setResultUrl] = useState(String);
   const [buttonType, setButtonType] = useState(TYPE_COMPRESS); //compress 일때 압축, copy 일때 복사
+
+
 
   return (
     <>
@@ -30,6 +33,7 @@ export default () => {
           resultUrl={resultUrl}
           buttonType={buttonType} />
       </section>
+
     </>
   );
 }
